66714b23b79f06f90a38d64389ea14955fa2eb19
0tsuu <minhnhut13@icloud.com>
2024-07-18 21:32:26 +0700

    make some changes

--- app/src/main/java/com/example/myapplication/sign_up.kt
+++ app/src/main/java/com/example/myapplication/sign_up.kt
@@ -103,7 +103,7 @@ class sign_up : AppCompatActivity() {
                     // Navigate to the next screen upon successful registration
                     goToSignInActivity()
                 } else {
-                    Log.e("Registration", "Registration failed")
+                    Log.e("Regist  ration", "Registration failed")
                     goToWelcomeActivity()
                     // Handle registration failure
                 }
