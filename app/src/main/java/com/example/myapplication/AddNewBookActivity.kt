a5cbf87a959448c11439d9194908d3924f8b949e
Xaoimiimii <gaococonut1199@gmail.com>
2024-08-31 21:31:12 +0700

    Add check id button

--- app/src/main/java/com/example/myapplication/AddNewBookActivity.kt
+++ app/src/main/java/com/example/myapplication/AddNewBookActivity.kt
@@ -2,22 +2,61 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
-import androidx.activity.enableEdgeToEdge
+import android.widget.EditText
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class AddNewBookActivity : AppCompatActivity() {
+
+    private lateinit var priceInput: EditText
+    private lateinit var seekBar: SeekBar
+    private lateinit var seekBarValue: TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_add_new_book)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+
+        priceInput = findViewById(R.id.price_input)
+        seekBar = findViewById(R.id.seekBar)
+        seekBarValue = findViewById(R.id.seekBarValue)
+
+        // Update EditText when SeekBar is changed
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                priceInput.setText(progress.toString())
+                seekBarValue.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Do nothing
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Do nothing
+            }
+        })
+
+        // Update SeekBar when EditText is changed
+        priceInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s != null && s.isNotEmpty()) {
+                    val value = s.toString().toInt()
+                    seekBar.progress = value
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Do nothing
+            }
+        })
     }
 
     fun goToDashboardActivity(view: View) {
