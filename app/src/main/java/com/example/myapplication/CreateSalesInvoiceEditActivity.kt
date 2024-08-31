0fd0732febb250c11a260e5f0297fbd47df832fd
Xaoimiimii <gaococonut1199@gmail.com>
2024-08-31 23:16:59 +0700

    Merge Nhu and main

--- app/src/main/java/com/example/myapplication/CreateSalesInvoiceEditActivity.kt
+++ app/src/main/java/com/example/myapplication/CreateSalesInvoiceEditActivity.kt
@@ -5,6 +5,8 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageButton
+import android.widget.SeekBar
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
