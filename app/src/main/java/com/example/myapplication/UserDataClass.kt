6a290ebdd3974ab39e14838b3143da0ecc61a36a
Xaoimiimii <gaococonut1199@gmail.com>
2024-08-31 16:21:11 +0700

    Staged and committed changes in AndroidManifest.xml, UserActivity.kt, and UserDataClass.kt

--- app/src/main/java/com/example/myapplication/UserDataClass.kt
+++ app/src/main/java/com/example/myapplication/UserDataClass.kt
@@ -4,10 +4,6 @@ data class User(
     var username: String,
     var email: String,
     var password: String,
-<<<<<<<< HEAD:app/src/main/java/com/example/myapplication/user_sign_up.kt
-    var confirm_password: String,
-    var token: String
-========
     var confirmPassword: String,
     var token: String
 )
@@ -15,5 +11,4 @@ data class User(
 data class Signin(
     val username: String,
     val password: String
->>>>>>>> Nhu:app/src/main/java/com/example/myapplication/UserDataClass.kt
 )
