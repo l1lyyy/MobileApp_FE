ff78329d8f3e3596c1958772f84be2d0e76cb9c9
Tran Khanh Nhu <141507912+Xaoimiimii@users.noreply.github.com>
2024-09-01 12:10:40 +0700

    Update CreateBookImportOderEditActivity.kt

--- app/src/main/java/com/example/myapplication/CreateBookImportOderEditActivity.kt
+++ app/src/main/java/com/example/myapplication/CreateBookImportOderEditActivity.kt
@@ -2,6 +2,8 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.View
 import android.widget.EditText
 import android.widget.SeekBar
@@ -21,11 +23,44 @@ class CreateBookImportOderEditActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_create_book_import_oder_edit)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
+
+        amountInput = findViewById(R.id.amount_input)
+        seekBar = findViewById(R.id.seekBar)
+        seekBarValue = findViewById(R.id.seekBarValue)
+
+        // Update EditText when SeekBar is changed
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                amountInput.setText(progress.toString())
+                seekBarValue.text = progress.toString()
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {
+                // Do nothing
+            }
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+                // Do nothing
+            }
+        })
+
+        // Update SeekBar when EditText is changed
+        amountInput.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // Do nothing
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s != null && s.isNotEmpty()) {
+                    val value = s.toString().toInt()
+                    seekBar.progress = value
+                }
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // Do nothing
+            }
+        })
     }
 
     fun goToBooksImportActivity(view: View) {
