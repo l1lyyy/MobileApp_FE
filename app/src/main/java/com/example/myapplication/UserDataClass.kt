4e382c5726bff2a72e59130bace66baf75edc3a5
Tran Khanh Nhu <141507912+Xaoimiimii@users.noreply.github.com>
2024-09-01 12:14:11 +0700

    Update UserDataClass.kt

--- app/src/main/java/com/example/myapplication/UserDataClass.kt
+++ app/src/main/java/com/example/myapplication/UserDataClass.kt
@@ -1,14 +1,22 @@
 package com.example.myapplication
 
+import android.media.session.MediaSession.Token
+import com.google.gson.annotations.SerializedName
+
 data class User(
     var username: String,
     var email: String,
     var password: String,
+    @SerializedName("confirm_password")
     var confirmPassword: String,
-    var token: String
 )
 
 data class Signin(
     val username: String,
-    val password: String
+    val password: String,
 )
+
+data class Token(
+    val token: String
+)
+
