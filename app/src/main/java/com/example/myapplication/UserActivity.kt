6aa54be644d336ee8abd8519c2ecc2e9feb78e7d
Xaoimiimii <gaococonut1199@gmail.com>
2024-09-01 12:02:34 +0700

    Fix setting and user in dashboard

--- app/src/main/java/com/example/myapplication/UserActivity.kt
+++ app/src/main/java/com/example/myapplication/UserActivity.kt
@@ -1,25 +1,20 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 
 class UserActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_account)
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-//            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-//            insets
-//        }
-    }
-
-    fun goToDashboardActivity(view: View) {
-        val intent = Intent(this, DashboardActivity::class.java)
-        startActivity(intent)
+        setContentView(R.layout.activity_user)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
     }
 }
