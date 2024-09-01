eb4c7ab8fdcef4dec93d385bbf9f2e7900ba978f
Tran Khanh Nhu <141507912+Xaoimiimii@users.noreply.github.com>
2024-09-01 12:14:48 +0700

    Update sign_up.kt

--- app/src/main/java/com/example/myapplication/sign_up.kt
+++ app/src/main/java/com/example/myapplication/sign_up.kt
@@ -2,7 +2,6 @@ package com.example.myapplication
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.EditText
 import android.widget.ImageButton
@@ -12,6 +11,10 @@ import androidx.appcompat.app.AppCompatActivity
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import android.util.Log
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import com.google.gson.Gson
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -19,7 +22,8 @@ class sign_up : AppCompatActivity() {
     lateinit var username: EditText
     lateinit var email: EditText
     lateinit var password: EditText
-    lateinit var confirmPassword: EditText
+    lateinit var confirm_password: EditText
+    //lateinit var signin: TextView
     lateinit var signup: ImageButton
 
     private lateinit var postApi: PostApi
@@ -32,7 +36,7 @@ class sign_up : AppCompatActivity() {
         username = findViewById(R.id.username_input)
         email = findViewById(R.id.email_input)
         password = findViewById(R.id.password_input)
-        confirmPassword = findViewById(R.id.confirm_password_input)
+        confirm_password = findViewById(R.id.confirm_password_input)
         signup = findViewById(R.id.sign_up_button)
 
         signup.setOnClickListener {
@@ -40,12 +44,12 @@ class sign_up : AppCompatActivity() {
             val username_res = username.text.toString()
             val email_res = email.text.toString()
             val password_res = password.text.toString()
-            val confirm_password_res = confirmPassword.text.toString()
+            val confirm_password_res = confirm_password.text.toString()
 
             if (validateInput(username_res, email_res, password_res, confirm_password_res)) {
                 Log.i("SignUp", "Validation passed")
-                val user = User(username_res, email_res, password_res, confirm_password_res, "")
-                sendUserData(user)
+                val user = User(username_res, email_res, password_res, confirm_password_res)
+                sendUserData(username_res, email_res, password_res,confirm_password_res)
                 Log.i("SignUp", "User data sent: $username_res, $email_res")
             } else {
                 Log.e("SignUp", "Validation failed")
@@ -80,17 +84,67 @@ class sign_up : AppCompatActivity() {
         }
     }
 
-    private fun sendUserData(user: User) {
-        // Initialize Retrofit
-        val retrofit = Retrofit.Builder()
-            .baseUrl(PostApi.BASE_URL)
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
+    fun goToSignInActivity()
+    {
+        val intent = Intent(this, sign_in::class.java)
+        startActivity(intent)
+    }
+//    private fun sendUserData(user: User) {
+//        // Khởi tạo Retrofit
+//        val retrofit = Retrofit.Builder()
+//            .baseUrl(PostApi.BASE_URL)
+//            .addConverterFactory(GsonConverterFactory.create())
+//            .build()
+//
+//        postApi = retrofit.create(PostApi::class.java)
+//
+//        val call = postApi.registrationUser(user)
+//        call.enqueue(object : Callback<User> {
+//            override fun onResponse(call: Call<User>, response: Response<User>) {
+//                Log.i("Thong tin dang nhap","gui thanh cong")
+//                if (response.isSuccessful) {
+//                    Log.i("Registration", "Registration successful")
+//                    // Navigate to the next screen upon successful registration
+//                    goToSignInActivity()
+//                } else {
+//                    Log.e("Registration", "Registration failed")
+//                    goToWelcomeActivity()
+//                    // Handle registration failure
+//                }
+//            }
+//
+//            override fun onFailure(call: Call<User>, t: Throwable) {
+//                Log.e("Registration", "Error: ${t.message}")
+//                // Handle failure to connect to server
+//            }
+//        })
+//    }
+    private fun sendUserData(
+        username: String,
+        email: String,
+        password: String,
+        confirmPassword: String,
+    ) {
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val httpClient = OkHttpClient.Builder()
+        httpClient.addInterceptor(logging)
+
+        val builder = Retrofit.Builder().baseUrl(PostApi.BASE_URL).addConverterFactory(GsonConverterFactory.create()).client(httpClient.build())
+        val retrofit = builder.build()
 
         postApi = retrofit.create(PostApi::class.java)
 
+        val user = User(username = username, email = email,password = password, confirmPassword =confirmPassword)
+
+        val gson = Gson()
+        val json = gson.toJson(user)
+        Log.d("User JSON",json)
+
         val call = postApi.registrationUser(user)
-        call.enqueue(object : Callback<User> {
+
+    call.enqueue(object : Callback<User> {
             override fun onResponse(call: Call<User>, response: Response<User>) {
                 if (response.isSuccessful) {
                     Log.i("Registration", "Registration successful")
